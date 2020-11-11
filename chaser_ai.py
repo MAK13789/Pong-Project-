b09bf398843a218507ed6b5d225f260b42045428
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2020-11-10 19:23:55 -0500

    Add files via upload

--- chaser_ai.py
+++ chaser_ai.py
@@ -1,34 +1,55 @@
 def pong_ai(paddle_frect, other_paddle_frect, ball_frect, table_size):
-    '''return "up" or "down", depending on which way the paddle should go to
-    align its centre with the centre of the ball, assuming the ball will
-    not be moving
-    
-    Arguments:
-    paddle_frect: a rectangle representing the coordinates of the paddle
-                  paddle_frect.pos[0], paddle_frect.pos[1] is the top-left
-                  corner of the rectangle. 
-                  paddle_frect.size[0], paddle_frect.size[1] are the dimensions
-                  of the paddle along the x and y axis, respectively
-    
-    other_paddle_frect:
-                  a rectangle representing the opponent paddle. It is formatted
-                  in the same way as paddle_frect
-    ball_frect:   a rectangle representing the ball. It is formatted in the 
-                  same way as paddle_frect
-    table_size:   table_size[0], table_size[1] are the dimensions of the table,
-                  along the x and the y axis respectively
-    
-    The coordinates look as follows:
-    
-     0             x
-     |------------->
-     |
-     |             
-     |
- y   v
-    '''          
-    
-    if paddle_frect.pos[1]+paddle_frect.size[1]/2 < ball_frect.pos[1]+ball_frect.size[1]/2:
-     return "down"
+    if paddle_frect.pos[1] + paddle_frect.size[1]/2 < ball_frect.pos[1] + ball_frect.size[1]/2:
+       return "down"
     else:
-     return "up"
+       return  "up"
+
+
+'''
+            if self.frect.intersect(paddle.frect):
+                if (paddle.facing == 1 and self.get_center()[0] < paddle.frect.pos[0] + paddle.frect.size[0]/2) or \
+                (paddle.facing == 0 and self.get_center()[0] > paddle.frect.pos[0] + paddle.frect.size[0]/2):
+                    continue
+
+                c = 0
+
+                while self.frect.intersect(paddle.frect) and not self.frect.get_rect().colliderect(walls_Rects[0]) and not self.frect.get_rect().colliderect(walls_Rects[1]):
+                    self.frect.move_ip(-.1*self.speed[0], -.1*self.speed[1], move_factor)
+
+                    c += 1
+                theta = paddle.get_angle(self.frect.pos[1]+.5*self.frect.size[1])
+
+
+                v = self.speed
+
+                v = [math.cos(theta)*v[0]-math.sin(theta)*v[1],
+                             math.sin(theta)*v[0]+math.cos(theta)*v[1]]
+
+                v[0] = -v[0]
+
+                v = [math.cos(-theta)*v[0]-math.sin(-theta)*v[1],
+                              math.cos(-theta)*v[1]+math.sin(-theta)*v[0]]
+
+
+                # Bona fide hack: enforce a lower bound on horizontal speed and disallow back reflection
+                if  v[0]*(2*paddle.facing-1) < 1: # ball is not traveling (a) away from paddle (b) at a sufficient speed
+                    v[1] = (v[1]/abs(v[1]))*math.sqrt(v[0]**2 + v[1]**2 - 1) # transform y velocity so as to maintain the speed
+                    v[0] = (2*paddle.facing-1) # note that minimal horiz speed will be lower than we're used to, where it was 0.95 prior to the  increase by 1.2
+
+                #a bit hacky, prevent multiple bounces from accelerating
+                #the ball too much
+                if not paddle is self.prev_bounce:
+                    self.speed = (v[0]*self.paddle_bounce, v[1]*self.paddle_bounce)
+                else:
+                    self.speed = (v[0], v[1])
+                self.prev_bounce = paddle
+
+
+                while c > 0 or self.frect.intersect(paddle.frect):
+
+                    self.frect.move_ip(.1*self.speed[0], .1*self.speed[1], move_factor)
+
+                    c -= 1
+
+                moved = 1
+'''
