4c32e307c3b85686328b57ef89ffc28c49df3151
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2020-11-10 19:24:11 -0500

    Add files via upload

--- pong_new_AI.py
+++ pong_new_AI.py
@@ -27,123 +27,100 @@
  y   v
     '''
 import PongAIvAI 
-
-intersected = False
-prev_sign = 0
+import time
+first_run = True
+prev_pos = [0,0]
+sign2 = 0
+y = 0
+counter = 0
+wall_collide = False
+frame_delay = True
 
 def new_ai(paddle_frect, other_paddle_frect, ball_frect, table_size):
-	global intersected
-	global prev_sign
-	print("djslfje")
-	print(ball_frect.speed[0])
-	#x_vel = ball_frect.speed[0]
-	sign = x_vel/abs(x_vel)
-	if (sign - prev_sign == 2):
-		print("intersected")
-	
+    global prev_pos
+    global first_run
+    global sign2
+    global y
+    global counter
+    global wall_collide
+    start_time = time.time()
+    # on the first frame, there is no previous position of the ball, so it is set to the middle
+    if first_run:
+        prev_pos = [table_size[0]/2, table_size[1]/2]
+        first_run = False        
 
-	prev_sign = sign
-	
-	'''
-	if intersected:
-		print("intersected")
-		intersected = False
-	elif intersect(other_paddle_frect, ball_frect):
-		intersected = True
-	'''
-   # if opponent's paddle intersects with the ball, then do this:
+    # find the position of the ball's center, and also 
+    pos = [ball_frect.pos[0] + ball_frect.size[0]/2, ball_frect.pos[1]+ ball_frect.size[1]/2]
+    vel = [pos[0] - prev_pos[0], pos[1] - prev_pos[1]]
 
-# call intersect_line
-# check if it's farther than the paddle position - if it is, use the previous line, and intersect it with the paddle x-pos to find the y-pos
+    if (vel[0] == 0):
+        sign = -1 # so that there is no intersection if vel is 0
+    else:
+        sign = vel[0]/abs(vel[0])
+    # (sign - sign2 == 2  or wall_collide) and 
+    if (sign > 0): #abs val or not - maybe for change of directions
+        # runs the following code either after it bounces off the opponent's paddle or at the start of a round if it's going towards your paddle
+        # only works if your paddle is on the right - the code needs to change based on what side of the board your paddle is on
+        if (False): # frame_delay
+            frame_delay = False
+        else:
+            print("run")
+            
+            final_pos = pos.copy()
+            predicted_prev_pos = final_pos
+            vel_new = vel.copy()
+            prev_vel = vel_new
+            counter = 0
+            
+            while (True):
+                # find the x-coordinate when the ball intersects a wall
+                if (vel_new[1] > 0):
+                    t = (table_size[1] - ball_frect.size[1]/2 - final_pos[1])/vel_new[1]
+                    final_pos = [final_pos[0] + t*vel_new[0], table_size[1] - ball_frect.size[1]/2]
+                elif (vel_new[1] < 0):
+                    t = (0 + ball_frect.size[1]/2 - final_pos[1]) / vel_new[1]
+                    final_pos = [final_pos[0] + t*vel_new[0], 0 + ball_frect.size[1]/2]
+                vel_new = [vel_new[0], -vel_new[1]]
+                end_time = time.time()
+                print ((end_time - start_time) * 1000)
+                if ((end_time - start_time) * 1000) > 0.1:
+                    break
+                # check if the point where it intersects a wall is to the right of the paddle, and exit the loop
+                if (final_pos[0] >= paddle_frect.pos[0]):
+                    break
+                #print(final_pos[0], "-----wall------", counter) # print the predicted position for when it intersects with a wall
+                predicted_prev_pos = final_pos
+                prev_vel = vel_new
+                counter += 1
+            # use the previous inersection point (before it passed the paddle's position) as the starting position, and intersect the line made from that with the paddle    
+            t = (paddle_frect.pos[0] - ball_frect.size[0]/2 - predicted_prev_pos[0])/prev_vel[0]
+            y = predicted_prev_pos[1] + t*prev_vel[1]
+            #print(y, "-------------------paddle----")
+            wall_collide = False
+    prev_pos = pos
+    sign2 = sign
 
-def intersect_line(vel, init_pos, table_size):
-	# pos = init_pos + t*vel
-	# t = (pos[1] - init_pos[1])/vel[1]
-	# if vel[1] > 0, find pos[0] when pos[1] = table_size[1]
-	if (vel[1] > 0):
-		t = (table_size[1] - init_pos[1])/vel[1]
-		return init_pos[0] + t*vel[0]
-	if (vel[1] < 0):
-		t = (0 - init_pos[1]) / vel[1]
-		return init_pos[0] + t*vel[0]
-	# if vel[1] = 0 - --   save for lalter
-	# if vel[1] < 0, find pos[0] when pos[1] = 0
+    # if it intersects with a wall
+    if (pos[1] >= table_size[1] - ball_frect.size[1]/2 or pos[1] <= ball_frect.size[1]/2) and sign > 0:
+        #print("--wall_pos--", pos[0])
+        wall_collide = True
+        pass
 
-def intersect_paddle(vel, init_pos):
-	t = (paddle_frect.pos[0] - init_pos[0])/vel[0]
-	return init_pos[1] + t*vel[1]
+    if pos[0]+ball_frect.size[0]/2 >= paddle_frect.pos[0] - 2:
+        # when it bounces off the right-hand side
+        #print(pos[1]-y, "--paddle error--", counter)
+        y = table_size[1]/2
+        #print("go to center")
 
-def reflect_line(vel):
-	return [vel[0], -vel[1]]
+    # if the ball goes out of bounds    
+    if pos[0]+ball_frect.size[0]/2 >= paddle_frect.pos[0] + 5 or pos[0]-ball_frect.size[0]/2 <= other_paddle_frect.pos[0] - 5:
+        y = table_size[1]/2
+        #print("go to center")
+        prev_pos = [table_size[0]/2, table_size[1]/2]
 
-def intersect(other_paddle_frect, ball_frect):
-        # two rectangles intersect iff both x and y projections intersect
-        for i in range(2):
-            if ball_frect.pos[i] < other_paddle_frect.pos[i]: # projection of self begins to the left  MAYBE ADD SOME CONSTANT
-                if other_paddle_frect.pos[i] >= ball_frect.pos[i] + ball_frect.size[i]:
-                    return 0
-            elif ball_frect.pos[i] > other_paddle_frect.pos[i]:
-                if ball_frect.pos[i] >= other_paddle_frect.pos[i] + other_paddle_frect.size[i]:
-                    return 0
-        return 1#ball_frect.size > 0 and other_paddle_frect.size > 0
-
-'''
-    def get_angle(self, y):
-        center = self.frect.pos[1]+self.size[1]/2
-        rel_dist_from_c = ((y-center)/self.size[1])
-        rel_dist_from_c = min(0.5, rel_dist_from_c)
-        rel_dist_from_c = max(-0.5, rel_dist_from_c)
-        sign = 1-2*self.facing
-
-        return sign*rel_dist_from_c*self.max_angle*math.pi/180
-
-
-
-
-            if self.frect.intersect(paddle.frect):
-                if (paddle.facing == 1 and self.get_center()[0] < paddle.frect.pos[0] + paddle.frect.size[0]/2) or \
-                (paddle.facing == 0 and self.get_center()[0] > paddle.frect.pos[0] + paddle.frect.size[0]/2):
-                    continue
-
-                c = 0
-
-                while self.frect.intersect(paddle.frect) and not self.frect.get_rect().colliderect(walls_Rects[0]) and not self.frect.get_rect().colliderect(walls_Rects[1]):
-                    self.frect.move_ip(-.1*self.speed[0], -.1*self.speed[1], move_factor)
-
-                    c += 1
-                theta = paddle.get_angle(self.frect.pos[1]+.5*self.frect.size[1])
-
-
-                v = self.speed
-
-                v = [math.cos(theta)*v[0]-math.sin(theta)*v[1],
-                             math.sin(theta)*v[0]+math.cos(theta)*v[1]]
-
-                v[0] = -v[0]
-
-                v = [math.cos(-theta)*v[0]-math.sin(-theta)*v[1],
-                              math.cos(-theta)*v[1]+math.sin(-theta)*v[0]]
+    if paddle_frect.pos[1] + paddle_frect.size[1]/2 < y:
+       return "down"
+    else:
+       return  "up"
 
 
-                # Bona fide hack: enforce a lower bound on horizontal speed and disallow back reflection
-                if  v[0]*(2*paddle.facing-1) < 1: # ball is not traveling (a) away from paddle (b) at a sufficient speed
-                    v[1] = (v[1]/abs(v[1]))*math.sqrt(v[0]**2 + v[1]**2 - 1) # transform y velocity so as to maintain the speed
-                    v[0] = (2*paddle.facing-1) # note that minimal horiz speed will be lower than we're used to, where it was 0.95 prior to the  increase by 1.2
-
-                #a bit hacky, prevent multiple bounces from accelerating
-                #the ball too much
-                if not paddle is self.prev_bounce:
-                    self.speed = (v[0]*self.paddle_bounce, v[1]*self.paddle_bounce)
-                else:
-                    self.speed = (v[0], v[1])
-                self.prev_bounce = paddle
-
-
-                while c > 0 or self.frect.intersect(paddle.frect):
-
-                    self.frect.move_ip(.1*self.speed[0], .1*self.speed[1], move_factor)
-
-                    c -= 1
-
-                moved = 1
-'''
